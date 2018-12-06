892528aaea16185c0e937e7a1a0a53d542b8f3eb
wang926454 <1107224733@qq.com>
2018-12-06 19:05:19 +0800

    更新打字机文字

--- text-auto-type/js/index.js
+++ text-auto-type/js/index.js
@@ -31,20 +31,19 @@ var addClass = function(element, className) {
 
 // 打字效果
 var arr = [
-    { type: 'text', text: '泻药，我家馍，又名静静，是个十足的女神经，嗯？' },
+    { type: 'text', text: '泻药，我家馍，又名静静，是个十足的女神经，额~' },
     { type: 'wait', time: 900 },
     { type: 'delete', num: 4 },
-    { type: 'text', text: '，嗯!' },
+    { type: 'text', text: '，嗯！' },
     { type: 'br' },
     { type: 'text', text: '她温柔美丽，善良大方' },
     { type: 'text', text: '，国色天香，沉鱼落雁，如花似玉，闭月羞花，贤良淑德，花容月貌，秋水伊人，一笑倾城，冰清玉洁，娇俏佳人，朱颜玉润，玉骨冰肌，窈窕淑女，美若天仙，一顾倾城，才智国人，出水芙蓉，阿娇金屋，闭月羞花，逞娇呈美，春暖花香，春色满园……', time: 50 },
     { type: 'br' },
     { type: 'text', text: '给你们看张她的照片吧~' },
     { type: 'wait', time: 900 },
-    { type: 'img', src: 'https://wang926454.gitee.io/reader/Image/headt.png', id: "cat", style: "width: 50%;display: block;margin-left: auto;margin-right: auto;margin-top: 20px;margin-bottom: 20px;" },
-    { type: 'wait', time: 900 },
-    // { type: 'delete', num: 1, time: 1000 },
-    { type: 'text', text: '是不是美美哒~' }
+    { type: 'br' },
+    { type: 'text', text: '嗯，想了一下，还是算了~毕竟我根本没有，想想就好了！' }
+    /* { type: 'img', src: 'https://wang926454.gitee.io/reader/Image/headt.png', id: "cat", style: "width: 100px;display: block;margin-left: auto;margin-right: auto;margin-top: 20px;margin-bottom: 20px;" }, */
 ]
 
 var autoType = new AutoType(content, arr);
